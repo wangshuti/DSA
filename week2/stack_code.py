d97c1fbc259907aee791b874f75ad298aa74c53d
wangshuti <44692430+wangshuti@users.noreply.github.com>
2019-10-03 15:48:45 +0800

    Update stack_code.py

--- week2/stack_code.py
+++ week2/stack_code.py
@@ -1,44 +1,28 @@
-Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+
 >>> class Stack(object):
 	def _init_(self):
 		self._list=[]
-
+#創立棧
 		
 >>> def push(self,item):
 	self._list.append(item)
-
+#新增一個到棧頂
 	
 >>> def pop(self):
 	return self._list.pop()
+#彈出
 
 >>> def peek(self):
 	if self._list:
 		return self._list[-1]
 	else:
 		return None
-
+#返回
 	
 >>> def is_empty(self):
 	return self._list == []
+#判斷是否為空
 
 >>> def size(self):
 	return len(self._list)
-
->>> if _name_ == '_main_':
-	s=Stack()
-	s.push(1)
-	s.push(2)
-	s.push(3)
-	s.push(4)
-	print(s.pop())
-	print(s.pop())
-	print(s.pop())
-	print(s.pop())
-
-	
-Traceback (most recent call last):
-  File "<pyshell#32>", line 1, in <module>
-    if _name_ == '_main_':
-NameError: name '_name_' is not defined
->>> 
+#返回元素個數
