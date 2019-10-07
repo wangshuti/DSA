233a62a084eef6171b3ca9a9adfe83f8d23a8d95
wangshuti <44692430+wangshuti@users.noreply.github.com>
2019-10-07 16:51:52 +0800

    Update and rename insertion sort_code.py to Insertion sort_code.py

--- week4/Insertion sort_code.py
+++ week4/Insertion sort_code.py
@@ -1,15 +1,5 @@
-Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> list = [20,9,100,0,55,3,11]
->>> def insertion_sort(list):
-	for i in range(1,len(list)):
-		tmp = list[i]
-		j = i-1
-		while j >= 0 and tmp < list[j]:
-			list[j+1] = list[j]
-			j = j-1
 
-			
 >>> def insertion_sort(list):
 	for i in range(1,len(list)):
 		tmp = list[i]
