4738a8515a9734538f0ddc0ca3adb9caf682f206
wangshuti <44692430+wangshuti@users.noreply.github.com>
2019-11-12 17:59:21 +0800

    Add files via upload

---
+++ HW2/merge_sort_05135902補交.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Solution(): 
+    def merge(self, left, right):
+        res = []
+        while left and right:
+            if left[0] < right[0]:
+                res.append(left.pop(0))
+            else:
+                res.append(right.pop(0))
+        res = res + left + right
+        return res
+
+    def merge_sort(self, lists):
+        if len(lists) <= 1:
+            return lists
+        mid = len(lists)//2
+        left = Solution().merge_sort(lists[:mid])
+        right = Solution().merge_sort(lists[mid:])
+        return Solution().merge(left,right)
+
+
+# In[2]:
+
+
+merge = Solution().merge_sort([3,2,-4,6,4,2,19])
+
+
+# In[3]:
+
+
+print(merge)
+
+
+# In[ ]:
+
+
+
+
